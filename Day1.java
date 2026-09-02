9f2504770a67e73523835c37e2712caa1c7e7f17
Novita <novitanovita81615@gmail.com>
2026-09-02 21:21:37 +0800

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // jenis komentar 1: selamat malam
+
+        /* jenis komentar 2:
+        perkenalkan nama saya novita
+        dari prodi sistem informasi */
+
+    }
+}
