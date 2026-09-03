3b6cef263b40c56dc277f15182c7785da465c6e2
Novita <novitanovita81615@gmail.com>
2026-09-03 12:02:32 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("BIODATA DIRI"); 
+        
+        // print 
+        System.out.print("Nama: " + "Novita");
+
+        // println
+        System.out.println();
+        System.out.println("Prodi: " + "Sistem Informasi");
+        System.out.println("Alamat: " + "Kecamatan mapilli, " + "desa segerang");
+        System.out.println("Asal sekolah: " + "smkn negeri campalagian");
+
+        // printf
+        System.out.printf("Umur: " + "%d tahun%n", 17);
+        System.out.printf("Tinggi: " + "%d cm", 149);
+    }
+}
