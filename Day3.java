35a2e315eb0d0372c932c53fef0ed6813a67c08c
Novita <novitanovita81615@gmail.com>
2026-09-04 16:30:31 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        // belajar ngoding day 3, penggunaan escape sequence
+        System.out.println("informatic\nstudyclub");
+        System.out.println("informatic\sstudy\sclub");
+        System.out.println("informatic\bstudyclub");
+        System.out.println("informatic\\studyclub");
+        System.out.println("informatic\"studyclub\"");
+        System.out.println("informatic\tstudyclub");
+        
+        }
+    }
