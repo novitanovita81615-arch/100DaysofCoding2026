7dd4139a34570a0a4cea24e6caaa670672a3173e
Novita <novitanovita81615@gmail.com>
2026-09-05 20:30:13 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        // byte = kecil
+        // short = agak besar
+        // int = besar
+        // long = sangat besar
+        // note = Semakin besar kapasitasnya, semakin besar angka yang bisa disimpan.
+        
+        byte a = 127;
+        short b = 32767;
+        int c = 2147483647;
+        long d = 18000000000000000L;
+
+        System.out.println("byte : " + a);
+        System.out.println("short : " + b);
+        System.out.println("int : " + c);
+        System.out.println("long : " + d);
+    }
+}
