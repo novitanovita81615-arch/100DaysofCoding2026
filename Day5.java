ea278c181bd5298b1df369e00f4a9ee222e6b422
Novita <novitanovita81615@gmail.com>
2026-09-06 20:35:58 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        float tinggi = 149f;
+        double nilai = 85.95;
+        float jarak = 4.7f;
+        double waktu = 2.57;
+        
+        System.out.println("tinggi : " + tinggi);
+        System.out.println("nilai : " + nilai);
+        System.out.println("jarak : " + jarak + " km");
+        System.out.println("waktu : " + waktu + " jam");
+        
+    }
+}
