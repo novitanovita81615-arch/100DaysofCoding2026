72b97a1b4e5ef5d1372efdc07ac56232816f4713
Novita <novitanovita81615@gmail.com>
2026-09-08 18:38:15 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        String nama = "novita";
+        String angkatan = "2026";
+        String univ = "universitas sulawesi barat";
+        
+        System.out.println("nama : "  + nama);
+        System.out.println("angkatan : "  + angkatan);
+        System.out.println("univ : "  + univ);
+        
+    }
+}
