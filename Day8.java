ba63bc6543e0bd9a22e1215cb554ea70167ff0cf
Novita <novitanovita81615@gmail.com>
2026-09-09 18:33:35 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // deklarasi
+        String nama;
+        int umur;
+        
+        // inisialisasi
+        nama = "orang 1";
+        umur = 17;
+        
+        System.out.println("nama : " + nama);
+        System.out.println("umur : " + umur);
+        
+        // updatean
+        nama = "orang 2";
+        umur = 18;
+        
+        System.out.println("nama : " + nama);
+        System.out.println("umur : " + umur);
+    }
+}
