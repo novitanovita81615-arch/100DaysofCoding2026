23cfd05920d7aa782aa90405892599a44982e050
Novita <novitanovita81615@gmail.com>
2026-09-10 19:01:11 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        final int umur = 17;
+        final int hari = 9;
+        final int bulan = 9;
+        System.out.println("umur : " + umur);
+        System.out.println("hari : " + hari);
+        System.out.println("bulan : " + bulan);
+        
+    }
+}
