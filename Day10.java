05f6f2616d732b42405c3de782af4eb70ceb0794
Novita <novitanovita81615@gmail.com>
2026-09-11 21:42:39 +0800

    Delete Day10.java

--- Day10.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Byte min : " + Byte.MIN_VALUE + " and Byte max : " + Byte.MAX_VALUE);
-        System.out.println("Short min : " + Short.MIN_VALUE + " and Short max : " + Short.MAX_VALUE);
-        System.out.println("Int min : " + Integer.MIN_VALUE + " and Int max : " + Integer.MAX_VALUE);
-        System.out.println("Long min : " + Long.MIN_VALUE + " and Long max : " + Long.MAX_VALUE);
-    }
-}
