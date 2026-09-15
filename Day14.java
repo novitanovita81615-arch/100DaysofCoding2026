c024eb3ec52831264ca5c89543889483d1abb39a
Novita <novitanovita81615@gmail.com>
2026-09-15 19:24:12 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int angka1 = 25;
+        int angka2 = 15;
+        
+        int hasil1 = angka1 + angka2;
+        int hasil2 = angka1 - angka2;
+        
+        System.out.println("hasil penjumlahan : " + hasil1);
+        System.out.println("hasil pengurangan : " + hasil2);
+    }
+}
