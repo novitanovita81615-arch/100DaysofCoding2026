3e6a8149384dc84cc5c66733d46b30479585daf7
Novita <novitanovita81615@gmail.com>
2026-09-21 19:47:51 +0800

    Update Day20.java

--- Day20.java
+++ Day20.java
@@ -4,23 +4,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        System.out.print("Masukkan angka : ");
+        System.out.print("masukkan angka : ");
         int angka = s.nextInt();
         String teks1 = String.valueOf(angka);
-        System.out.print("Masukkan pecahan : ");
+        System.out.print("masukkan pecahan : ");
         double pecahan = s.nextDouble();
         String teks2 = String.valueOf(pecahan);
-        System.out.print("Masukkan huruf : ");
+        System.out.print("masukkan huruf : ");
         char huruf = s.next().charAt(0);
         String teks3 = String.valueOf(huruf);
-        System.out.print("Masukkan boolean : ");
+        System.out.print("masukkan boolean : ");
         boolean benar = s.nextBoolean();
         String teks4 = String.valueOf(benar);
 
-        System.out.println("Hasil String : " + teks1);
-        System.out.println("Hasil String : " + teks2);
-        System.out.println("Hasil String : " + teks3);
-        System.out.println("Hasil String : " + teks4);
+        System.out.println("hasil string : " + teks1);
+        System.out.println("hasil string : " + teks2);
+        System.out.println("hasil string : " + teks3);
+        System.out.println("hasil string : " + teks4);
         
     }
 }
